1c37664a18252522f972a69297a8522d38dc46cb
周泽飞 <zefei.zhou@tongdun.net>
2022-04-06 14:38:46 +0800

    主题调整

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,7 @@
 import { create } from '@storybook/theming';
 import { addons } from '@storybook/addons';
+import theme from "./theme";
 
 addons.setConfig({
-  theme: create({ colorPrimary: 'hotpink', colorSecondary: '#146bfb' }),
+  theme: theme //create({ colorPrimary: 'hotpink', colorSecondary: '#146bfb' }),
 });
---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: '中台业务组件库',
+  brandUrl: 'https://tdfe.github.io/tddc/lerna-resource/index.html',
+  brandImage: 'https://place-hold.it/350x150',
+  colorPrimary: '#126BFB', 
+  colorSecondary: '#5a98fc' 
+});
