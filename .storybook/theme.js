268c0ae567a0bdaaaa00a2652413a472a41a6343
周泽飞 <zefei.zhou@tongdun.net>
2022-04-06 14:43:22 +0800

    主题调整

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -4,7 +4,7 @@ export default create({
   base: 'dark',
   brandTitle: '中台业务组件库',
   brandUrl: 'https://tdfe.github.io/tddc/lerna-resource/index.html',
-  brandImage: 'https://place-hold.it/350x150',
+  brandImage: 'https://raw.githubusercontent.com/TDFE/tddc/main/logo.jpg',
   colorPrimary: '#126BFB', 
   colorSecondary: '#5a98fc' 
 });
